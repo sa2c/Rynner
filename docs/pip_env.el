7093bb766d3b9b1272ff932c454374aef3e46efe
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-08-02 17:49:20 +0100

    pip_env.el

---
+++ docs/pip_env.el
@@ -0,0 +1,13 @@
+(progn
+   (setenv "PYTHONDONTWRITEBYTECODE" "1")
+   (setenv "PIP_PYTHON_PATH" "/usr/local/Cellar/pipenv/11.9.0/libexec/bin/python3.6")
+   (setenv "PYTHONUNBUFFERED" "1")
+   (setenv "__CF_USER_TEXT_ENCODING" "0x1F8:0x0:0x2")
+   (setenv "VIRTUAL_ENV" "/Users/markdawson/.local/share/virtualenvs/rynner-BD0iQRUE")
+   (setenv "PIPENV_ACTIVE" "1")
+   (setenv "PATH"
+     (concat 
+        "/Users/markdawson/.local/share/virtualenvs/rynner-BD0iQRUE/bin:/usr/local/Cellar/pipenv/11.9.0/libexec/tools:"
+        (getenv "PATH"))))
+
+

f7ba1a6b96c6531728e595738b7d59bff1c7a727
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-08-06 17:40:20 +0100

    documentation changes

--- docs/example.py
+++ docs/example.py
@@ -2,7 +2,7 @@
 A possible use of the API
 '''
 from RESET.InputTypes import *
-from RESET import Job, JobType
+from RESET import Job, JobType, SlurmRunner
 import RESET
 from my_app_library import lib
 
@@ -15,62 +15,52 @@ executable = RESET.GitRepo(
     git_repo_url, build='make parallel', exe='bin/parallel')
 
 # UI stuff for job configuration
-_interface = {
-    JobName:
-    TextField(),
-    PlotType:
-    OptionField({
+interface = {
+    'JobName': TextField(),
+    'PlotType': OptionField({
         # How to serialise arbitrary objects as options? Option name? Pickle?
         'Line': lib.plot_line,
         'Bar': lib.plot_bar,
         'Pie': lib.plot_pie
     }).set_label('Plot Type'),
-    Velocity:
-    NumericField(10),
-    WorkingDir:
-    Directory(),  # maybe camel case in directory is auto-parsed?
-    localDataFile:
-    File(default=lib.local_data_default),
-    localConfigFile:
-    File(default='path/to/local/file'),
-    config_date:
-    HiddenField(datetime.datetime.now, on=HiddenField.ACCEPT),
-    commitID:
-    GitCommitField(git_repo_url),
-    mode:
-    54  # add some any serialisable, constant data here
+    'Velocity': NumericField(10),
+    'WorkingDir': Directory(),  # maybe camel case in directory is auto-parsed?
+    'localDataFile': File(default=lib.local_data_default),
+    'localConfigFile': File(default='path/to/local/file'),
+    'config_date': HiddenField(datetime.datetime.now, on=HiddenField.ACCEPT),
+    'commitID': GitCommitField(git_repo_url),
+    'runner' : SelectRunnable([SlurmRunner,ClusterRunner])
 }
 
 # Should config remember the last state or each option?
 
-template_files = {'SlurmTypes': 'template_path', 'PBSTypes': 'template_path'}
-JobType.register_templates(template_files)
-
-
 #defaults setup is: lambda (data, runner): runner.job().config(data)
 # Sets up the job object
-def setup(data, runner):
-
-    job = runner.job()
-
+def setup(data):
     # set up some filesystem behaviour
-    job.download(local=data['WorkingDir'])
-    job.upload(local=data['WorkingDir'], remote=[])
-    job.download(
-        remote=['/some/remote/path'],
-        local=[data['localDataFile']],
-        interval=1000)  # follow/sync file
 
+    job = data['runner'].run()
+    job.download([(remote, local), (remote, local)])
+    job.download(remote=remote, local=local, interval=1000)   # interval in minutes
+    job.upload([(local, remote)])
+    job.walltime('12:24:13')                                 # can I do anything clever on AWS here?
+    job.num_nodes(1)                                         # used for provisioning on AWS
+    job.memory(1)                                            # used for selecting nodes on AWS
     job.bandwidth(1000)
 
-    jobcard_variables = {
+    job.params({
         'dimensions': 2,
         'cmd': executable.path(commit=data['commitID'])
-    }
+    })
 
-    job.config(jobcard_variables)
+    if runner.is(SlurmRunner):
+        job.options('time', '23432', 'dependents', 1)
+        if runner.is(PBSRunner):
+        job.options('time', '23432', 'dependents', 1)
+    if runner.is(AWSRunner):
+        job.options('time', '23432', 'dependents', 1)
 
-    return job
+    jobID = job.run()                                      # Freeze job object, any setters after this point have no effect
 
 
 def plot_results(data):
@@ -82,9 +72,12 @@ def do_postprocess(data):
     app_lib.expensive_data_operation(data['localDataFile'], data['mode'])
 
 
-job_type = JobType(
-    setup, config_interface,
-    executable)  # executable is optional? can be set on Job object
+job_type = JobType(setup, config_interface)
+
+job_type.register_templates({
+    SlurmRunner: 'template_path',
+    PBSRunner: 'template_path'
+})
 
 # add a postprocessing
 job_type.add_action('Plot Results', plot_results)
@@ -92,6 +85,7 @@ job_type.add_action('Plot Results', plot_results)
 # add a postprocessing action with dependency
 plot_action = job_type.add_action('Plot Results', plot_results)
 job_type.add_action('Run postprocessing', do_postprocess, depends=plot_action)
+job_type.supported_runners(SlurmRunner, ClusterRunner, AWSRunner)
 
 register_job_type('My Job Type', job_type)
 

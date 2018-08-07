3d85a896e7738b01db2cf2db02bcf0144706f613
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-08-07 14:26:43 +0100

    refactor Run to delegate to runner.behaviour

--- run.py
+++ run.py
@@ -1,32 +1,26 @@
-from restricted_dict import RestrictedDict
-
-
-class InvalidDownloadOptionException(ValueError):
-    pass
-
-
-class InvalidUploadOptionException(ValueError):
-    pass
-
-
-class RunnerNotSpecifiedException(AttributeError):
-    pass
-
-
 class Run:
+
+    # TODO support additional options for runners (via a call to behaviour?)
+
     __valid_download_arguments = ['interval']
     __valid_upload_arguments = []
-    __allowed_options = {
-        'walltime', 'jobcard', 'memory', 'num_nodes', 'bandwidth'
-    }
 
-    def __init__(self, options=None, runner=None, template=None):
-        '''options should be None or an object of type dict, these options are intended to be used to configure the runner'''
+    def __init__(self, data, runner=None, options={}, template=None):
         self.__downloads = []
         self.__uploads = []
-        self.options = RestrictedDict(options, allowed=self.__allowed_options)
         self.runner = runner
         self.template = template
+        self.data = data
+
+        if self.runner is None:
+            raise RunnerNotSpecifiedException
+
+        self.__behaviour_context = runner.behaviour.get_context()
+        self.__runner_context = runner.get_context()
+
+        for key, value in options.items():
+            method = getattr(self, key)
+            method(value)
 
     ########################### File Upload/Download lists ############################
 
@@ -40,36 +34,39 @@ class Run:
                                 self.__valid_upload_arguments,
                                 InvalidUploadOptionException, **kwargs)
 
-    ############################# Option Setters ##############################
+    ########################## Runner Option Setters ##########################
+
+    def bandwidth(self, bandwidth):
+        # TODO - this is passed to runner, not behaviour
+        self.runner.bandwidth(self.__runner_context, bandwidth)
+
+    ######################## Behaviour Option Setters #########################
 
     def walltime(self, seconds=0, minutes=0, hours=0):
-        self.options['walltime'] = seconds + minutes * 60 + hours * 60 * 60
+        self.runner.behaviour.walltime(
+            self.__behaviour_context, seconds + minutes * 60 + hours * 60 * 60)
+
+    def memory(self, mb=0, kb=0, gb=0):
+        self.runner.behaviour.memory(self.__behaviour_context,
+                                     kb + mb * 1024 + gb * 1024**2)
+
+    def num_nodes(self, num_nodes):
+        self.runner.behaviour.num_nodes(self.__behaviour_context, num_nodes)
+
+    def script(self, script):
+        self.runner.behaviour.script(self.__behaviour_context, script)
+
+    ############################# Utility Methods #############################
 
     def from_template(self, args, template=None):
         if template is None:
             template = self.template
-        self.options['jobcard'] = template.render(args)
-
-    def jobcard(self, string):
-        self.options['jobcard'] = string
-
-    def memory(self, mb, kb, gb):
-        self.options['memory'] = kb + mb * 1025 + gb * 1025**2
-
-    def bandwidth(self, bandwidth):
-        self.options['bandwidth'] = bandwidth
-
-    def num_nodes(self, num_nodes):
-        self.options['num_nodes'] = num_nodes
+        self.script(template.render(args))
 
     ############################# Private methods #############################
 
-    def __get_option(self, option_name):
-        return self.options[option_name]
-
     def __add_to_file_list(self, first, second, filelist, valid_args,
                            exception, **kwargs):
-        '''add a download, with a gien set of options'''
         if type(first) is str and type(second) is str:
             first = [first]
             second = [second]
@@ -91,9 +88,7 @@ class Run:
     ################################# Actions #################################
 
     def run(self):
-        if self.runner is None:
-            raise RunnerNotSpecifiedException
-        return self.runner.run(self.options, self.downloads, self.uploads)
+        return self.runner.run(self.__runner_context, self.__behaviour_context)
 
     ############################# Property Lists ##############################
 
@@ -104,3 +99,15 @@ class Run:
     @property
     def uploads(self):
         return self.__uploads
+
+
+class InvalidDownloadOptionException(ValueError):
+    pass
+
+
+class InvalidUploadOptionException(ValueError):
+    pass
+
+
+class RunnerNotSpecifiedException(AttributeError):
+    pass
--- test_run.py
+++ test_run.py
@@ -5,7 +5,12 @@ from run import *
 
 class TestRun(unittest.TestCase):
     def setUp(self):
-        self.run = Run()
+        self.mock_data = MagicMock()
+        self.mock_runner = MagicMock()
+        self.run = Run(self.mock_data, self.mock_runner)
+
+        self.bcontext = self.mock_runner.behaviour.get_context()
+        self.rcontext = self.mock_runner.get_context()
 
 
 class TestRunDownload(TestRun):
@@ -103,120 +108,136 @@ class TestRunOptions(TestRun):
     def test_walltime_adds_to_options(self):
         walltime = 10000  # walltime in seconds
         self.run.walltime(walltime)
-        assert self.run.options['walltime'] == walltime
+        self.mock_runner.behaviour.walltime.assert_called_once_with(
+            self.bcontext, walltime)
 
     def test_walltime_minutes(self):
         walltime = 10000  # walltime in seconds
         self.run.walltime(minutes=walltime)
-        assert self.run.options['walltime'] == walltime * 60
+        self.mock_runner.behaviour.walltime.assert_called_once_with(
+            self.bcontext, walltime * 60)
 
     def test_walltime_hours(self):
         walltime = 10000  # walltime in seconds
         self.run.walltime(hours=walltime)
-        assert self.run.options['walltime'] == walltime * 60 * 60
+        self.mock_runner.behaviour.walltime.assert_called_once_with(
+            self.bcontext, walltime * 60 * 60)
 
     def test_walltime_compound_time(self):
         sec = 123  # walltime in seconds
         minute = 456
         hour = 789
         self.run.walltime(seconds=sec, minutes=minute, hours=hour)
-        assert self.run.options[
-            'walltime'] == sec + (minute * 60) + (hour * 60 * 60)
+        self.mock_runner.behaviour.walltime.assert_called_once_with(
+            self.bcontext, sec + (minute * 60) + (hour * 60 * 60))
 
     def test_set_custom_default_options(self):
-        opts = {'walltime': '12000'}
-        self.run = Run(opts)
-        assert self.run.options == opts
+        walltime = 1234
+        self.run = Run(
+            self.mock_data,
+            self.mock_runner,
+            options={
+                'walltime': walltime,
+                'memory': 10
+            })
+        self.mock_runner.behaviour.walltime.assert_called_once_with(
+            self.bcontext, walltime)
+        self.mock_runner.behaviour.memory.assert_called_once_with(
+            self.bcontext, 1024 * 10)
 
     def test_options_fail_invalid_opts(self):
         with self.assertRaises(Exception):
-            self.run = Run({'invalid_options': 'invalid'})
+            self.run = Run(self.mock_data, options,
+                           {'invalid_options': 'invalid'})
 
     def test_modifying_options_raises_exception(self):
-        self.run = Run()
+        self.run = Run(self.mock_data, self.mock_runner)
         with self.assertRaises(Exception):
             self.run.options['invalid_option'] = 'invalid'
 
     def test_bandwidth_option(self):
         arg = MagicMock
         self.run.bandwidth(arg)
-        assert self.run.options['bandwidth'] == arg
+        self.mock_runner.bandwidth.assert_called_once_with(self.rcontext, arg)
 
     def test_num_nodes_option(self):
         arg = MagicMock
         self.run.num_nodes(arg)
-        assert self.run.options['num_nodes'] == arg
+        self.mock_runner.behaviour.num_nodes.assert_called_once_with(
+            self.bcontext, arg)
 
     def test_memory_option(self):
         arg = MagicMock
         self.run.memory(arg)
-        assert self.run.options['memory'] == arg
+        self.mock_runner.behaviour.memory.assert_called_once_with(arg)
 
     def test_memory_option(self):
         arg = MagicMock
         self.run.memory(kb=5, mb=10, gb=25)
-        assert self.run.options[
-            'memory'] == 5 + (10 * 1025) + (25 * 1025 * 1025)
+        self.mock_runner.behaviour.memory.assert_called_once_with(
+            self.bcontext, 5 + (10 * 1024) + (25 * 1024 * 1024))
 
 
-class TestRunInterfaces(unittest.TestCase):
+class TestRunInterfaces(TestRun):
     def test_template_render_args(self):
         template = MagicMock()
         args = MagicMock()
-        self.run = Run(template=template)
+        self.run = Run(self.mock_data, self.mock_runner, template=template)
         self.run.from_template(args)
 
         template.render.assert_called_once_with(args)
 
-    def test_jobcard_set_from_template_render_output(self):
+    def test_script_set_from_template_render_output(self):
         template = MagicMock()
         args = MagicMock()
-        self.run = Run(template=template)
+        self.run = Run(self.mock_data, self.mock_runner, template=template)
         self.run.from_template(args)
 
-        assert self.run.options['jobcard'] == template.render()
+        self.mock_runner.behaviour.script.assert_called_once_with(
+            self.bcontext, template.render())
 
-    def test_jobcard_set_from_template_raises_exception_when_no_template(self):
-        self.run = Run()
+    def test_script_set_from_template_raises_exception_when_no_template(self):
+        self.run = Run(self.mock_data, self.mock_runner)
         args = MagicMock()
         with self.assertRaises(Exception):
             self.run.from_template(args)
 
     def test_from_template_takes_template_argument(self):
-        self.run = Run()
+        self.run = Run(self.mock_data, self.mock_runner)
         template = MagicMock()
         args = MagicMock()
-        self.run = Run(template=template)
+        self.run = Run(self.mock_data, self.mock_runner, template=template)
         self.run.from_template(args)
 
         template.render.assert_called_once_with(args)
 
-    def test_jobcard_set_from_string_if_provided(self):
+    def test_script_set_from_string_if_provided(self):
         template = MagicMock()
         string = MagicMock()
-        run = Run(template=template)
-        run.jobcard(string)
+        run = Run(self.mock_data, self.mock_runner, template=template)
+        run.script(string)
 
-        assert run.options['jobcard'] == string
+        self.mock_runner.behaviour.script.assert_called_once_with(
+            self.bcontext, string)
 
     def test_run_can_run(self):
-        runner_inst = MagicMock()
-        run = Run(runner=runner_inst)
+        run = Run(self.mock_data, runner=self.mock_runner)
         jobID = run.run()
-        runner_inst.run.assert_called_once_with(run.options, run.downloads,
-                                                run.uploads)
+        self.mock_runner.run.assert_called_once_with(self.rcontext,
+                                                     self.bcontext)
 
     def test_run_run_returns_return_value_of_runner(self):
-        runner_inst = MagicMock()
-        run = Run(runner=runner_inst)
+        run = Run(self.mock_data, runner=self.mock_runner)
         jobID = run.run()
 
-        assert jobID == runner_inst.run()
+        assert jobID == self.mock_runner.run()
 
     def test_run_errors_without_runner(self):
-        run = Run()
         with self.assertRaises(RunnerNotSpecifiedException):
-            run.run()
+            run = Run(self.mock_data)
+
+    def test_stores_data_in_datastore(self):
+        assert self.run.data == self.mock_data
 
 
 if __name__ == '__main__':

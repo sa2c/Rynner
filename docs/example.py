0a3a7391fe1f1a3f44a7b8fc2536f0461f3f56fe
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-08-08 17:12:54 +0100

    changes to examples

--- docs/example.py
+++ docs/example.py
@@ -16,51 +16,77 @@ executable = RESET.GitRepo(
 
 # UI stuff for job configuration
 interface = {
-    'JobName': TextField(),
-    'PlotType': OptionField({
+    'JobName':
+    TextField(),
+    'PlotType':
+    OptionField({
         # How to serialise arbitrary objects as options? Option name? Pickle?
         'Line': lib.plot_line,
         'Bar': lib.plot_bar,
         'Pie': lib.plot_pie
     }).set_label('Plot Type'),
-    'Velocity': NumericField(10),
-    'WorkingDir': Directory(),  # maybe camel case in directory is auto-parsed?
-    'localDataFile': File(default=lib.local_data_default),
-    'localConfigFile': File(default='path/to/local/file'),
-    'config_date': HiddenField(datetime.datetime.now, on=HiddenField.ACCEPT),
-    'commitID': GitCommitField(git_repo_url),
-    'runner' : SelectRunnable([SlurmRunner,ClusterRunner])
+    'Velocity':
+    NumericField(10),
+    'WorkingDir':
+    Directory(),  # maybe camel case in directory is auto-parsed?
+    'localDataFile':
+    File(default=lib.local_data_default),
+    'localConfigFile':
+    File(default='path/to/local/file'),
+    'config_date':
+    HiddenField(datetime.datetime.now, on=HiddenField.ACCEPT),
+    'commitID':
+    GitCommitField(git_repo_url),
+    'cluster':
+    SelectRunner(
+    )  # specifies types of runners...but the ACTUAL list is elsewhere...
 }
 
 # Should config remember the last state or each option?
+
 
 #defaults setup is: lambda (data, runner): runner.job().config(data)
 # Sets up the job object
 def setup(data):
     # set up some filesystem behaviour
 
-    job = data['runner'].run()
-    job.download([(remote, local), (remote, local)])
-    job.download(remote=remote, local=local, interval=1000)   # interval in minutes
-    job.upload([(local, remote)])
-    job.walltime('12:24:13')                                 # can I do anything clever on AWS here?
-    job.num_nodes(1)                                         # used for provisioning on AWS
-    job.memory(1)                                            # used for selecting nodes on AWS
-    job.bandwidth(1000)
+    # data['runner'] is a SPECIFIC RUNNER INSTANCE, which is associated with the cluster we specify...
 
-    job.from_template({
+    run = Run(data['cluster'])  # or Run(HostAdapter(scheduler='slurm'))
+    # job interface is FIXED for everyone
+    # runner is a string key for data, or an object instance with a run method
+    # runner can be an instance of anything on which we can call run with the job for submission
+
+    store = run.id  # use this to "connect" to later if you need to, shouldn't need to...ignore for now
+
+    run.download([('remote', 'local'),
+                  ('remote', 'local')])  # <= adds to Runner under job
+    run.download(
+        remote='remote', local='local', interval=1000)  # interval in minutes
+    run.upload([('local', 'remote')])
+    run.walltime('12:24:13')  # can I do anything clever on AWS here?
+    run.num_nodes(1)  # used for provisioning on AWS
+    run.memory(1)  # used for selecting nodes on AWS
+    run.bandwidth(1000)
+
+    run.from_template({
         'dimensions': 2,
         'cmd': executable.path(commit=data['commitID'])
     })
 
-    if runner.is(SlurmRunner):
-        job.options('time', '23432', 'dependents', 1)
-    if runner.is(PBSRunner):
-        job.options('time', '23432', 'dependents', 1)
-    if runner.is(AWSRunner):
-        job.options('time', '23432', 'dependents', 1)
+    # equivalent....
+    run.data['new_data'] = 'new value'
+    data['new_data'] = 'new value'
 
-    return job # return job, so that parent can do the running
+    # additional cluster-specific examples
+    if run.slurm:
+        run.options('time', '23432', 'dependents', 1)
+    if run.pbs:
+        run.options('time', '23432', 'dependents', 1)
+    if run.aws:
+        run.options('time', '23432', 'dependents', 1)
+
+    return run  # return run, so that parent can do the running
 
 
 def plot_results(data):

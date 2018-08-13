8adb3d9d806b8e6841bb4a795c042eeadcd1a0be
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-08-13 16:51:49 +0100

    rynner view

--- rynner_view.py
+++ rynner_view.py
@@ -1,61 +1,85 @@
-class RynnerView:
-    WidgetInstanceClass = ...
-    widget_args = ...
-    validation = ...
+import sys
+from PySide2.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QApplication, QTextEdit, QVBoxLayout
+from PySide2.QtCore import QSize
 
-    def __init__(self, key, label, validation=..., instance_class=..., widget_args=..., **kwargs):
-        self. ... = ...
 
-    def build(self, gui-tk-symbol):
-        '''build method'''
-        # ...either returns itself it it's a leaf or recurses if it's branch
-        # looks up the appropriate type to return based on gui-tk-symbol key
-        map kwargs -> widget-args
+# an empty/blank QWidget wrapper?
+class TextInput(QWidget):
+    def __init__(self, key, label, parent=None, validation=None, default=None):
+        super().__init__(parent)
+        self.input = QTextEdit()
+        self.validation = validation
+        self.key = key
+        self.default = default
 
-        widget = self.WidgetInstanceClass(**widget_args, label=... validation=VALIDATION, mapping=MAPPING)
+        self.setLayout(QVBoxLayout())
 
-        return widget
+        self.init()
 
-class QRynnerWidget(QWidget):
-    '''The thing which is constructed'''
-    def __init__(type, validation, mapping):
-        self... = ...
+    def value(self):
+        return self.input.toPlainText()
 
     def valid(self):
-        pass
+        if self.validation is not None:
+            return self.validation(self.value())
+        else:
+            return True
 
-    def value(self):
-        pass
+    def init(self):
+        self.input.setText("tet")
+        #self.addWidget(self.input)
+        #self.label = QLabel("My Label")
+        #self.addWidget(self.label)
 
-    def draw(self): #? probably not....
-        pass
+    def addWidget(self, widget):
+        self.layout().addWidget(widget)
 
-    def value(self):
-        return -> same structure as before, a list of (machine_key, value) pairs something else
 
-class RynnerCommandLineWidget(QWidget):
-    def __init__(type, validation, mapping):
-        self... = ...
+#     def __init__(self,
+#                  key,
+#                  label,
+#                  *args,
+#                  parent=None,
+#                  validation=None,
+#                  **kwargs):
 
-    def valid(self):
-        pass
+#         super().__init__(parent)
+#         self.key = key
+#         self.label = label
+#         self.input = None
 
-    def value(self):
-        pass
+#     # This is called by the parent, if there is one, setting "parent"
 
-    def run(self): #? probably yes in this case....
-        pass
+#     def init(self, *args, **kwargs):
+#         # override this function if necessary
+#         # NEED TO ENSURE THIS IS OVERRIDEN...This creates examples
+#         self.input = QTextEdit(self)
+#         self.label = QLabel("My Label")
+#         self.addWidget(self.label)
+#         self.addWidget(self.input)
 
-class QRynnerDialog(QWidget):
-    def __init__(self, child):
+#     def valid(self):
+#         ## stores and returns validation (so the QWidget does not need to)
+#         if validation is not None:
+#             return self.validation(self.value())
 
-    def value(self):
-        flatten(child.value())
-        # check for duplicate keys at output list construction
+#     def value(self):
+#         #if leaf
+#         return self.input.value()
 
-class RynnerCommandLineManager(QWidget):
-    def __init__(self, child):
+#         #if parent
+#         return [child.value for child in self.children]
 
-    def value(self):
-        flatten(child.value())
-        # check for duplicate keys at output list construction
+#     def cli(self):
+#         valid = False
+#         while not valid:
+#             print("Label: {}".format(self.label))
+#             # read from command line
+
+#         return value
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = TextInput('key', 'Test Input')
+    widget.show()
+    sys.exit(app.exec_())
--- tests/test_rynner_view.py
+++ tests/test_rynner_view.py
@@ -1,17 +1,94 @@
 import unittest
-from unittest.mock import patch, MagicMock, call, ANY
+from unittest.mock import MagicMock as MM
+from unittest.mock import patch
+from PySide2.QtTest import QTest
 from rynner_view import *
 
+app = QApplication(sys.argv)
 
-class TestRynnerView(unittest.TestCase):
+
+class TestTextInput(unittest.TestCase):
     def setUp(self):
         pass
 
-    def instantiate(self, *args):
-        return RynnerView(*args)
+    def instance(self, **kwargs):
+        self.input = TextInput('key', 'Label', **kwargs)
+
+    def type_text(self, text):
+        QTest.keyClicks(self.input.input, text)
 
     def test_instance(self):
-        self.instantiate()
+        self.instance()
 
-    def test_next(self):
+    def test_can_type_text(self):
+        self.instance()
+        self.type_text("some input")
+
+    def test_value_method_return_text(self):
+        self.instance()
+        self.type_text("some input")
+        self.assertEqual(self.input.value(), "some input")
+
+    def test_validate_returns_true_by_default(self):
+        self.instance()
+        self.type_text("some input")
+        self.assertTrue(self.input.valid())
+
+    def test_validate_return_value(self):
+        mock_text = "some input"
+        validation = lambda input: input == mock_text
+        self.instance(validation=validation)
+        self.type_text("some input")
+        self.assertTrue(self.input.valid())
+        self.type_text("some input")
+        self.assertFalse(self.input.valid())
+
+    def test_validate_validation_is_called(self):
+        validation = MM()
+        self.instance(validation=validation)
+        self.type_text("some input")
+        self.input.valid()
+        validation.assert_called_once_with("some input")
+
+    def test_input_knows_its_key(self):
+        key = MM()
+        input = TextInput(key, 'label')
+        self.assertEqual(input.key, key)
+
+    def test_show_qwidget(self):
+        input = TextInput('key', 'label')
+        input.show()
+
+    def test_show_call_init_with_parent(self):
+        mock_parent = QWidget()
+
+        input = TextInput('key', 'label', parent=mock_parent)
+        self.assertEqual(input.parent(), mock_parent)
+
+    def test_show_call_init_with_none_by_default(self):
+        mock_parent = None
+
+        input = TextInput('key', 'label', parent=mock_parent)
+        self.assertEqual(input.parent(), mock_parent)
+
+    def test_sets_layout(self):
+        input = TextInput('key', 'label')
+        self.assertIsInstance(input.layout(), QVBoxLayout)
+
+    def test_has_addWidget_method(self):
+
+        input = TextInput('key', 'label')
+        mock_widget = QWidget()
+
+        assert mock_widget not in input.children()
+
+        input.addWidget(mock_widget)
+
+        assert mock_widget in input.children()
+
+    def test_sets_default_text_in_text_edit(self):
+        input = TextInput('key', 'label', default='default string')
+        self.assertEqual(input.value(), 'default string')
+
+    def my_test_QTextEdit_is_child(self):
         pass

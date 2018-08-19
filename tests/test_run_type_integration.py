e99edcb518db9a7b49a3d2e26914d1177e2d01b7
Mark Dawson <mark.dawson@swansea.ac.uk>
2018-08-19 19:36:47 +0100

    integration testing

--- tests/test_run_type_integration.py
+++ tests/test_run_type_integration.py
@@ -6,34 +6,30 @@ from tests.qtest_helpers import QTestCase
 from PySide2.QtTest import QTest
 
 
-class TestRunTypeIntegration(unittest.TestCase):
-    @unittest.skip('expected failure')
-    # workaround: unittest.expectedFailure does not work with nose
-    def test_configure_and_run_empty_runner(self):
-        interface = Interface([
+class TestRunTypeIntegration(QTestCase):
+    def setUp(self):
+        self.interface = Interface([
             TextInput('key', 'My Label', default="My Default"),
             TextInput(
                 'another_key', 'My Other Label', default="My Other Default"),
         ])
+        self.runner = lambda data: None
 
-        runner = lambda data: None
-        some_action = lambda data: None
+    def create_run_type(self):
+        self.run_type = RunType(self.runner, self.interface)
 
-        run_type = RunType(runner, interface)
+    def test_configure_and_run_empty_runner(self):
+        self.create_run_type()
 
-        run_type.add_action('Some Action', some_action)
+        self.assertNotQVisible(self.run_type.interface.dialog)
 
-        run_type.create()
+        self.run_type.create()
 
-        # The dialog window not created (unless in pdb) - need a test for this.
-        assert False
-
-    @unittest.skip('expected failure')
-    def test_dialog_window_has_title(self):
-        assert False
+        self.assertQVisible(self.run_type.interface.dialog)
 
     @unittest.skip('expected failure')
-    def test_dialog_window_test_behaviour_title(self):
+    def test_dialog_window_test_behaviour(self):
+        # - window title
         # - validation
         # - reset values
         # - multiple initialisations same widget

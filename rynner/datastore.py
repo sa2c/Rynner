50631fdaa77fa621eaaaaa219015b18d2cf9dc30
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-08-22 13:34:43 +0100

    datastore modified

--- rynner/datastore.py
+++ rynner/datastore.py
@@ -7,3 +7,7 @@ class Datastore:
 
     def jobs(self, id, run_type=None):
         raise NotImplementedError()
+
+    def set_connection(self, connection):
+        raise NotImplementedError()
+        self.connection = connection

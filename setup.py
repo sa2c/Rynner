4b28ae2b4dc84ac95ea1a593eaee78957459778e
Jarno Rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2019-02-06 11:21:09 +0000

    A simple setup script with little metadata

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='Rynner',
+        version='0.0.0',
+        url='https://github.com/M4rkD/Rynner/',
+        py_modules=['rynner'],
+    )

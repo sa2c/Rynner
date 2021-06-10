18526895a99dff6080eccd3fde020e109dd4ff5c
Vladimir Khodygo <vladimir.khodygo@swansea.ac.uk>
2021-06-10 11:24:12 +0100

    UPD: add custom username/domain input to example

--- simple.py
+++ simple.py
@@ -2,33 +2,46 @@ from parsl.channels import SSHChannel
 from parsl.providers.slurm.slurm import SlurmProvider
 from rynner.rynner import Rynner
 
-provider = SlurmProvider(
-    'compute',
-    channel=SSHChannel(
-        hostname='sunbird.swansea.ac.uk',
-        username='s.user.name',
-        script_dir='/tmp'
-    ),
-    nodes_per_block=1,
-    # tasks_per_node=1,  # fixme doesnt exist anymore?
-    init_blocks=1,
-    max_blocks=1,
-)
 
-rynner = Rynner(provider)
+username = input("Enter your username: ")
+domain = input("Enter the domain: ")
 
-run = rynner.create_run(
-    script='cat Makefile > tmps', uploads=['Makefile'], downloads=['tmps'])
-rynner.upload(run)
-print('upload')
-print(run)
-rynner.submit(run)
-print('submit')
-print(run)
+connect_ = input(f'Connect as {username} at {domain}? [Yy/Yes/No/Nn]:')
+if connect_.lower() not in ('y', 'yes', ''):
+    print('No connection established, exiting.')
+    exit(1)
+else:
+    provider = SlurmProvider(
+        'compute',
+        channel=SSHChannel(
+            hostname=domain,
+            username=username,
+            script_dir='/tmp'
+        ),
+        nodes_per_block=1,
+        # tasks_per_node=1,  # fixme doesn't exist anymore?
+        init_blocks=1,
+        max_blocks=1
+    )
 
-runs = [run]
-rynner.update(runs)
-print('runs')
-print(runs)
+    rynner = Rynner(provider)
 
-rynner.download(run)
+    run = rynner.create_run(
+        script='cat Makefile > tmps', uploads=['Makefile'], downloads=['tmps'])
+
+    rynner.upload(run)
+    print('upload')
+
+    print(run)
+    rynner.submit(run)
+
+    print('submit')
+    print(run)
+
+    runs = [run]
+    rynner.update(runs)
+    print('runs')
+    print(runs)
+
+    rynner.download(run)
+

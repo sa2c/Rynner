21e43731f991eddb84679750082f0691abc1f9f9
Vladimir Khodygo <vladimir.khodygo@swansea.ac.uk>
2021-06-24 21:05:35 +0100

    MAINT: rynner type hints/annotations

--- rynner/rynner.py
+++ rynner/rynner.py
@@ -5,31 +5,35 @@ from stat import S_ISDIR
 import time
 
 from pathlib import PurePosixPath
-from rynner.data_classes import Run
+
 import threading
 from copy import deepcopy
+
+from parsl.providers.slurm.slurm import SlurmProvider
+from parsl.providers.provider_base import JobStatus
+from typing import Optional, Union
+
+from rynner.pattern_parser import InvalidContextOption
+from rynner.data_classes import Run
 
 
 class Rynner:
 
-    StatusPending = 'PENDING'
-    StatusRunning = 'RUNNING'
-    StatusCancelled = 'CANCELLED'
-    StatusCompleted = 'COMPLETED'
+    StatusPending: str = 'PENDING'
+    StatusRunning: str = 'RUNNING'
+    StatusCancelled: str = 'CANCELLED'
+    StatusCompleted: str = 'COMPLETED'
 
-    StatesPreComplete = [StatusPending, StatusRunning]
-    StatesPostComplete = [StatusCompleted, StatusCancelled]
+    StatesPreComplete: list[str] = [StatusPending, StatusRunning]
+    StatesPostComplete: list[str] = [StatusCompleted, StatusCancelled]
 
-    def __init__(self, provider, path='rynner'):
-        self.provider = provider
-        self.path = path
+    def __init__(self, provider: SlurmProvider, path: str = 'rynner') -> None:
+        self.provider: SlurmProvider = provider
+        self.path: str = path
 
-    def create_run(self,
-                   script,
-                   uploads=None,
-                   downloads=None,
-                   namespace=None,
-                   jobname='rynner'):
+    def create_run(self, script: str, uploads: list[str] = None,
+                   downloads: list[str] = None, namespace: str = None,
+                   jobname: str = 'rynner') -> Run:
         if not uploads:
             uploads = []
 
@@ -46,13 +50,13 @@ class Rynner:
 
         return run
 
-    def _remote_dir(self, namespace, uid):
+    def _remote_dir(self, namespace: str, uid: str) -> PurePosixPath:
         p_ = PurePosixPath(self.path)
         path = p_.joinpath(namespace, uid) if namespace else p_.joinpath(uid)
         return path
 
     @staticmethod
-    def _parse_path(file_transfer):
+    def _parse_path(file_transfer: Union[str, list[str]]) -> tuple[str, str]:
         if type(file_transfer) == str:
             src = file_transfer
             dest, _ = os.path.split(file_transfer)
@@ -67,7 +71,8 @@ class Rynner:
 
         return src, dest
 
-    def list_local_files(self, run, local_source, remote_dir):
+    def list_local_files(self, run: Run, local_source: str,
+                         remote_dir: str) -> Union[list[str], list[list[str]]]:
         """Build a list of files a remote folder
         """
         expanded_uploads = []
@@ -88,7 +93,7 @@ class Rynner:
             print("No such file")
         return expanded_uploads
     
-    def start_upload(self, run: Run):
+    def start_upload(self, run: Run) -> None:
         """Spawn a thread to upload the files in the upload list.
         Update a report of the current state of the process.
         """
@@ -96,7 +101,7 @@ class Rynner:
         for upload in run.uploads:
             uploads += self.list_local_files(run, upload[0], upload[1])
 
-        def upload_thread(run_):  # fixme remove this nest?, verify name changes
+        def upload_thread(run_: Run) -> None:  # fixme remove this nest?, verify name changes
             """The function executed by the download thread
             """
             run_copy = deepcopy(run_)
@@ -106,13 +111,12 @@ class Rynner:
                 self.upload(run_copy)
                 
             run_.upload_status = 1.0
-            return
         
         run.upload_status = 0
         thread = threading.Thread(target=upload_thread, args=(run,))
         thread.start()
 
-    def upload(self, run):
+    def upload(self, run: Run) -> None:
         """Uploads files using provider channel.
         """
 
@@ -126,7 +130,8 @@ class Rynner:
 
         run.upload_time = time.time()
 
-    def list_remote_files(self, run, remote_source, local_dir):
+    def list_remote_files(self, run: Run, remote_source: str,
+                          local_dir: str) -> Union[list[str], list[list[str]]]:
         """Build a list of files a remote folder
         """
         expanded_downloads = []
@@ -141,7 +146,7 @@ class Rynner:
                 file_list = sftp_client.listdir(path=src)
                 for filename in file_list:
                     src = remote_source + '/' + filename
-                    # fixme os indepedent path
+                    # fixme posixpath
                     expanded_downloads += self.list_remote_files(run, src, dest)
             else:
                 expanded_downloads = [[remote_source, local_dir]]
@@ -151,7 +156,7 @@ class Rynner:
             print("No such file")
         return expanded_downloads
     
-    def start_download(self, run):  # fixme download and upload are identical?
+    def start_download(self, run: Run) -> None:  # fixme download and upload are identical?
         """Spawn a thread to download the files in the download list.
         Update a report of the current state of the process.
         """
@@ -159,7 +164,7 @@ class Rynner:
         for download in run.downloads:
             downloads += self.list_remote_files(run, download[0], download[1])
 
-        def download_thread(run_):
+        def download_thread(run_: Run) -> None:
             """The function executed by the download thread
             """
             run_copy = deepcopy(run_)
@@ -169,13 +174,12 @@ class Rynner:
                 self.download(run_copy)
                 
             run_.download_status = 1.0
-            return
         
         run.download_status = 0
         thread = threading.Thread(target=download_thread, args=(run,))
         thread.start()
 
-    def download(self, run):  # fixme same for download/upload
+    def download(self, run: Run) -> None:  # fixme same for download/upload
         """Download files using provider channel.
         """
 
@@ -194,7 +198,7 @@ class Rynner:
 
             self.provider.channel.pull_file(src, dest)
 
-    def submit(self, run):
+    def submit(self, run: Run) -> bool:
         # copy execution script to remote
 
         runscript_name = f'rynner_exec_{run.job_name}'
@@ -239,13 +243,13 @@ class Rynner:
             self.save_run_config(run)
             return True
 
-    def cancel(self, run):
+    def cancel(self, run: Run) -> None:
         self._record_time('cancel', run)
         run.job_id = self.provider.cancel(run.script, 1)
         run.job_status = self.StatusCancelled
         self.save_run_config(run)
 
-    def _record_time(self, label, run, execute=False):
+    def _record_time(self, label: str, run: Run, execute: bool = False) -> str:
         times_file = run.remote_dir.joinpath('rynner.times').as_posix()
         remote_cmd = f'echo "{label}: $(date +%s)" >> {times_file}'
         if execute:
@@ -253,7 +257,9 @@ class Rynner:
 
         return remote_cmd
 
-    def _finished_since_last_update(self, runs):
+    def _finished_since_last_update(self,
+                                    runs: list[Run]) -> tuple[list[bool],
+                                                              list[JobStatus]]:
 
         job_ids = [run.job_id for run in runs]
         status = self.provider.status(job_ids)
@@ -266,7 +272,7 @@ class Rynner:
 
         return needs_update, status
 
-    def read_time(self, run):
+    def read_time(self, run: Run) -> Optional[str]:
 
         filename = 'rynner.times'
         filepath = run.remote_dir.joinpath(filename).as_posix()
@@ -281,7 +287,7 @@ class Rynner:
             print(f"Exception happened: {e}")
             return None
 
-    def update(self, runs):
+    def update(self, runs: list[Run]) -> bool:
         """Performs an in-place update of run information. Only information in
         info and status keys are changed. Status is changed to reflect the
         current state of the job. Info is updated with the output of
@@ -318,7 +324,7 @@ class Rynner:
 
         return changed
 
-    def save_run_config(self, run):
+    def save_run_config(self, run: Run) -> str:
         """Saves the run configuration on the cluster
         """
 
@@ -332,7 +338,7 @@ class Rynner:
 
         return filepath
 
-    def get_runs(self, namespace=''):
+    def get_runs(self, namespace: str = '') -> list[Run]:
         """Read pickled run files from the cluster
         """
 

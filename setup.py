8afa1f8875c268db34f193e1d59974a9606d1fab
Michele Mesiti <mmesiti@users.noreply.github.com>
2022-05-04 11:37:15 +0200

    Adding 'tests' to py_modules in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
         name='Rynner',
         version='0.0.1',
         url='https://github.com/sa2c/Rynner',
-        py_modules=['rynner'],
+        py_modules=['rynner','tests'],
         install_requires=['parsl', 'paramiko']  # todo improve setup
     )

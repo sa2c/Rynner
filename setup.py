61ad5fccea5c6f57fd853fa21e7cc662d2fef655
TummyPan <Dr.Tianyi.Pan@outlook.com>
2023-01-24 15:20:12 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Rynner',
+    version='0.0.1',
+    url='https://github.com/sa2c/Rynner',
+    py_modules=['rynner'],
+    install_requires=['parsl', 'paramiko']
+)

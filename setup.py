72004878af32e54d0c4aa72a6c190b3f1f4caf92
TummyPan <Dr.Tianyi.Pan@outlook.com>
2023-01-27 14:38:46 +0000

    adjust setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from distutils.core import setup
 
+from setuptools import find_packages
+
+install_requires = [
+    "paramiko",
+    "parsl",
+]
+
 setup(
-    name='Rynner',
-    version='0.0.1',
-    url='https://github.com/sa2c/Rynner',
-    py_modules=['rynner'],
-    install_requires=['parsl', 'paramiko']
+    name="rynner",
+    version="0.0.1",
+    url="https://github.com/sa2c/Rynner",
+    packages=find_packages(),
+    install_requires=install_requires,
+    py_modules=["rynner"],
 )
